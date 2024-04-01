08c4e0b3c56b44136074f6e1abf27ad55e905818
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-04-02 00:29:50 +0530

    some ui updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,14 +35,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
-    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
-    implementation("com.google.firebase:firebase-analytics")
-
-    //fireBase realtime Database
-    implementation("com.google.firebase:firebase-database")
-
 }
