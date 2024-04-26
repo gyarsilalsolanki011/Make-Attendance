d4608abd474779e561f506ba1e46a19ad40960eb
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-04-26 18:36:28 +0530

    Data updated on AttendanceViewActivity

--- app/src/main/java/com/gyarsilalsolanki011/make_attendance/activities/AttendanceViewActivity.java
+++ app/src/main/java/com/gyarsilalsolanki011/make_attendance/activities/AttendanceViewActivity.java
@@ -2,6 +2,8 @@ package com.gyarsilalsolanki011.make_attendance.activities;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.gyarsilalsolanki011.make_attendance.R;
 import com.gyarsilalsolanki011.make_attendance.activities.adapter.RecyclerAttendanceViewAdapter;
@@ -16,6 +19,7 @@ import com.gyarsilalsolanki011.make_attendance.activities.modal.AttendanceViewMo
 import com.gyarsilalsolanki011.make_attendance.databinding.ActivityAttendanceViewBinding;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class AttendanceViewActivity extends AppCompatActivity {
     ArrayList<AttendanceViewModal> arrayListAttendance;
@@ -23,6 +27,7 @@ public class AttendanceViewActivity extends AppCompatActivity {
     private final FirebaseFirestore database = FirebaseFirestore.getInstance();
     private  final FirebaseAuth Auth = FirebaseAuth.getInstance();
     RecyclerAttendanceViewAdapter adapter;
+    String userid;
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,61 @@ public class AttendanceViewActivity extends AppCompatActivity {
 
         //setOnRecyclerView();
 
+        TextView classAttended1, classConducted1;
+        binding.MachineLearning.setText(SubjectType.MachineLearning.toString());
+        classAttended1 = findViewById(R.id.classes_attended1);
+        classConducted1 = findViewById(R.id.classes_conducted1);
+
+        setDataOnAttendanceViewActivity(SubjectType.MachineLearning.toString(), classAttended1, classConducted1);
+
+
+        TextView  classAttended2, classConducted2;
+        binding.ImageProcessing.setText(SubjectType.ImageProcessing.toString());
+        classAttended2 = findViewById(R.id.classes_attended2);
+        classConducted2 = findViewById(R.id.classes_conducted2);
+
+        setDataOnAttendanceViewActivity(SubjectType.ImageProcessing.toString(), classAttended2, classConducted2);
+
+
+        TextView  classAttended3, classConducted3;
+        binding.MobileComputing.setText(SubjectType.MobileComputing.toString());
+        classAttended3 = findViewById(R.id.classes_attended3);
+        classConducted3 = findViewById(R.id.classes_conducted3);
+
+        setDataOnAttendanceViewActivity(SubjectType.MobileComputing.toString(), classAttended3, classConducted3);
+
+
+        TextView  classAttended4, classConducted4;
+        binding.ComputerNetworking.setText(SubjectType.ComputerNetworking.toString());
+        classAttended4 = findViewById(R.id.classes_attended4);
+        classConducted4 = findViewById(R.id.classes_conducted4);
+
+        setDataOnAttendanceViewActivity(SubjectType.ComputerNetworking.toString(), classAttended4, classConducted4);
+
+
+        TextView  classAttended5, classConducted5;
+        binding.SoftwareEngineering.setText(SubjectType.SoftwareEngineering.toString());
+        classAttended5 = findViewById(R.id.classes_attended5);
+        classConducted5 = findViewById(R.id.classes_conducted5);
+
+        setDataOnAttendanceViewActivity(SubjectType.SoftwareEngineering.toString(), classAttended5, classConducted5);
+    }
+
+
+    private void setDataOnAttendanceViewActivity(String collection, TextView classAttended, TextView classConducted) {
+        userid = Objects.requireNonNull(Auth.getCurrentUser()).getUid();
+        DocumentReference documentReference = database.collection(collection).document(userid);
+        documentReference.addSnapshotListener(this, (value, error) -> {
+            if (error != null){
+                Log.e("Firebase database error", Objects.requireNonNull(error.getMessage()));
+                return;
+            }
+
+            assert value != null;
+            classConducted.setText("   6");
+            classAttended.setText("   5");
+
+        });
     }
 
     public void setOnRecyclerView() {
